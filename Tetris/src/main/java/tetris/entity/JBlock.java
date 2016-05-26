c036f86796ece28cd7067cb1f3adff64a14c93bb
Joni Luhtalampi <joni.luhtalampi@gmail.com>
2016-05-26 19:25:13 +0300

    määrittelyvaiheen luokkakaavio lisätty

--- Tetris/src/main/java/tetris/entity/JBlock.java
+++ Tetris/src/main/java/tetris/entity/JBlock.java
@@ -3,19 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tetris.domain;
+package tetris.entity;
 
 /**
  *
  * @author luhtalam
  */
-public class LBlock extends Block {
+public class JBlock extends Block {
 
-    public LBlock(int x, int y) {
-        super(x,y);
+    public JBlock(int x, int y) {
+        super(x, y);
         parts[0] = (new Part(x, y - 1));
         parts[1] = (new Part(x, y));
         parts[2] = (new Part(x, y + 1));
-        parts[3] = (new Part(x + 1, y + 1));
+        parts[3] = (new Part(x - 1, y + 1));
     }
 }
--- Tetris/src/main/java/tetris/entity/ZBlock.java
+++ Tetris/src/main/java/tetris/entity/ZBlock.java
@@ -3,19 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tetris.domain;
+package tetris.entity;
 
 /**
  *
  * @author luhtalam
  */
-public class TBlock extends Block {
+public class ZBlock extends Block {
 
-    public TBlock(int x, int y) {
+    public ZBlock(int x, int y) {
         super(x,y);
-        parts[0] = (new Part(x, y - 1));
+        parts[0] = (new Part(x + 1, y));
         parts[1] = (new Part(x, y));
-        parts[2] = (new Part(x - 1, y));
-        parts[3] = (new Part(x + 1, y));
-    }
+        parts[2] = (new Part(x, y - 1));
+        parts[3] = (new Part(x - 1, y - 1));
+    } 
 }
